89c1098c927b17b61499cd8f6485e49ec68d65c9
drejc <drey.ray@gmail.com>
2017-03-06 11:14:06 +0100

    Maven pom.xml clean up

--- src/main/java/com/zandero/rest/guice/RestFilterModule.java
+++ src/main/java/com/zandero/rest/guice/RestFilterModule.java
@@ -4,7 +4,8 @@ import com.google.inject.AbstractModule;
 import com.zandero.rest.*;
 
 /**
- * Basic RestFilter module to include
+ * Basic RestFilter module to include in project
+ * or bind manually as desired
  */
 public class RestFilterModule extends AbstractModule {
 
@@ -12,13 +13,15 @@ public class RestFilterModule extends AbstractModule {
 	protected void configure() {
 
 		// events async execution
-		bind(RestEventThreadPool.class).to(RestEventThreadPoolImpl.class);
+		bind(RestEventThreadPool.class).to(RestEventThreadPoolImpl.class); // optional for asynchronous event execution, one can bind his own thread pool if desired
 
 		// exception handling
-		bind(RestExceptionMapper.class);
+		bind(RestExceptionMapper.class); // mandatory for exception event filtering
 
-		// request and response filter
-		bind(RestRequestEventFilter.class);
-		bind(RestResponseEventFilter.class);
+		// request filter
+		bind(RestRequestEventFilter.class); // optional in case request duration log should be added
+
+		// response filter
+		bind(RestResponseEventFilter.class); // mandatory for event filtering
 	}
 }
